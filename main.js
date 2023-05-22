3f8d49385747a312992af7ce2039784e8b8ec301
lorenasandoval88 <lorenasandoval88@gmail.com>
2023-05-22 13:27:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,45 @@ function display(){
         console.log(box.value)
         localStorage.setItem('value',box.value)
         //header.innerHTML = box.value;
-        header.innerHTML = localStorage.getItem('value')
+        header.innerHTML = localStorage.getItem('user1')
+        //header.innerHTML = localStorage.getItem('value')
+
 }
 
+
+
+  function giveUsers() {
+
+    for (let i = 0; i < 3; i++) {
+        console.log("i",i)
+    fetch('https://randomuser.me/api/')
+      .then(function (response) {
+        console.log("response",response)
+        return response.json();
+    })
+  
+    .then(function (data) {
+      showUsers(data.results);
+      console.log("data",data)
+
+    })
+  
+    .catch(function (error) {
+      console.log("error", error);
+    });
+  
+    let showUsers = (users) => {
+        users.forEach(user => {       
+      
+          localStorage.setItem('user1', JSON.stringify(user));  
+      
+        });
+      };  
+      console.log("localStorage.getItem('user')",localStorage.getItem('user1'));  
+
+    }}
+  if(localStorage.getItem('user1')){
+    console.log("line47 if(localStorage.getItem(user1",localStorage.getItem('user1'));
+  }
+
+  giveUsers()
